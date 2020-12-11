fc19b3549276cf1cbfb1f18c49e19686651c1b80
jnli <jnli@secusoft.cc>
2020-12-11 17:45:40 +0800

    add: 项目初始化

---
+++ src/test/java/com/scaffolding/demo/ScaffoldingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaffolding.demo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScaffoldingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
